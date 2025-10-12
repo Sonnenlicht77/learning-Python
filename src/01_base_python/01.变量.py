338f1e7c8b067b178039e93570d5b814d8b186a0
yangx2 <sonnenlicht@foxmail.com>
2025-10-12 22:14:36 +0800

    测试

--- src/01_base_python/01.变量.py
+++ src/01_base_python/01.变量.py
@@ -39,17 +39,3 @@ print(list3)
 #[[1, 1], [2, 1, 'a'], [3, 1, False], [4, 2, 'TOM'], [5, 1, 'hello worldTOM'], 30]
 
 
-# 函数
-def add(a,b):
-    """
-     计算两个数的和。
-
-     Args:
-         a (int): 第一个加数。
-         b (int): 第二个加数。
-
-     Returns:
-         int: 两个加数的和。
-     """
-    return a+b
-print(add(1,2))
