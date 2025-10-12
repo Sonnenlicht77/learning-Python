11412b4cb58775dd0a515e2c0fb6398988cc0e1d
yangx2 <sonnenlicht@foxmail.com>
2025-10-12 22:28:50 +0800

    测试2

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-    base:'/learning-Python/',
+    base:"/learning-Python/",
   title: "Learning Python",
   description: "A VitePress Site",
   themeConfig: {
